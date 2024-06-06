93c706c11857451cf6ff00f893bc825619997eef
anhld90 <anhld90@gmail.com>
2024-06-06 21:19:56 +0700

    layouts

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <title>Acuas - Drinking Water Website Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wdth,wght@0,75..100,300..800;1,75..100,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    @vite(['resources/css/style.css', 'resources/css/bootstrap.min.css', 'resources/lib/animate/animate.min.css',
+    'resources/lib/owlcarousel/assets/owl.carousel.min.css',])
+</head>
